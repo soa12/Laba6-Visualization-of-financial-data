e98e1b04534de35cbf2a1737b7027aad33249df6
Sabyrzhan <aspombitov.sabyrzhan@gmail.com>
2017-11-21 04:43:20 +0700

    Начальная настройка, реализован модуль преобразования данных

---
+++ DataConversion/ConversionFromCSV.cs
@@ -0,0 +1,55 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConversion
+{
+    public class ConversionFromCSV : IConversion
+    {
+        private string path;
+        private List<CurrencyRate> currencyRatesList;
+
+        public ConversionFromCSV(string path)
+        {
+            this.path = path;
+            currencyRatesList = new List<CurrencyRate>();
+        }
+
+        public List<CurrencyRate> ToObject()
+        {
+            string line;
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] words = line.Split(',');                        
+                        currencyRatesList.Add(new CurrencyRate()
+                        {
+                            Ticker = words[0],
+                            Per = words[1],
+                            Date = DateTime.Parse(words[2]),
+                            Time = DateTime.Parse(words[3]),
+                            Open = Double.Parse(words[4]),
+                            High = Double.Parse(words[5]),
+                            Low = Double.Parse(words[5]),
+                            Close = Double.Parse(words[5]),
+                            Vol = Int32.Parse(words[6])                          
+                        });
+                    }
+                }
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return currencyRatesList;
+        }
+    }
+}
---
+++ DomainModel/CurrencyRate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class CurrencyRate
+    {
+        //<TICKER>,<PER>,<DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>
+        //EURRUB,60,20171120,010000,69.4562000,69.6904000,69.4456000,69.6839000,59
+        public string Ticker { get; set; }
+
+        public string Per { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public double Open { get; set; }
+
+        public double High { get; set; }
+
+        public double Low { get; set; }
+
+        public double Close { get; set; }
+
+        public int Vol { get; set; }
+    }
+}
