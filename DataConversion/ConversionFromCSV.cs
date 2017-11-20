5f3c844cfda6b4301c95406a91d100e74e1d72ac
Sabyrzhan <aspombitov.sabyrzhan@gmail.com>
2017-11-21 06:13:47 +0700

    исправлены ошибки при конвертирование из string в double

--- DataConversion/ConversionFromCSV.cs
+++ DataConversion/ConversionFromCSV.cs
@@ -1,6 +1,7 @@
 ﻿using DomainModel;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace DataConversion
         private string path;
         private List<CurrencyRate> currencyRatesList;
 
+        private double temp = 1.111;
         public ConversionFromCSV(string path)
         {
             this.path = path;
@@ -28,19 +30,24 @@ namespace DataConversion
                 {
                     while ((line = sr.ReadLine()) != null)
                     {
-                        string[] words = line.Split(',');                        
+                        string[] words = line.Split(',');
+                        //System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");                     
                         currencyRatesList.Add(new CurrencyRate()
                         {
                             Ticker = words[0],
                             Per = words[1],
                             Date = DateTime.Parse(words[2]),
                             Time = DateTime.Parse(words[3]),
-                            Open = Double.Parse(words[4]),
-                            High = Double.Parse(words[5]),
-                            Low = Double.Parse(words[5]),
-                            Close = Double.Parse(words[5]),
-                            Vol = Int32.Parse(words[6])                          
+                            Open = Double.Parse(words[4], new CultureInfo("en-US")),
+                            High = Double.Parse(words[5], new CultureInfo("en-US")),
+                            Low = Double.Parse(words[6], new CultureInfo("en-US")),
+                            Close = Double.Parse(words[7], new CultureInfo("en-US")),
+                            Vol = Int32.Parse(words[8])        
                         });
+
+                        //System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ru_RU");
+                        //currencyRatesList[currencyRatesList.Count - 1].Date = DateTime.Parse(words[2]);
+                        //currencyRatesList[currencyRatesList.Count - 1].Time = DateTime.Parse(words[3]);
                     }
                 }
             }
---
+++ DataConversionTest/ConversionFromCSVTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataConversion;
+using DomainModel;
+
+namespace DataConversionTest
+{
+    [TestClass]
+    public class ConversionFromCSVTest
+    {
+        [TestMethod]
+        public void TestMethodToObject()
+        {
+            string path = @"D:\Новая папка\Учеба\4 курс\Проектирование ИС и ИТ\Лаба 6\Laba6\EURRUB_171120_171120.csv";
+            string path1 = @"D:\Новая папка\Учеба\4 курс\Проектирование ИС и ИТ\Лаба 6\Laba6\EURRUB_171120_171120(1).csv";
+            ConversionFromCSV testClass = new ConversionFromCSV(path1);
+            List<CurrencyRate> forCheckList = testClass.ToObject();
+            //CurrencyRate forCheckCurrencyRate = forCheckList.FirstOrDefault();
+
+            Assert.IsNotNull(forCheckList);
+        }
+    }
+}
